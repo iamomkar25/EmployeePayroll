9c236f2207bf9d0ccb6a2277b557a9cd9be8b6e2
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-13 00:49:18 +0530

    insert-value

--- EmployeeServices.sql
+++ EmployeeServices.sql
@@ -1,8 +1,11 @@
 use payroll;
-create Table empoyee_payrolls1
-(
-id int identity(1,1) primary key,
-name varchar(200) not null,
-salary float,
-startDate date
-)
+
+
+Insert into empoyee_payrolls1(name,salary,startDate) values
+('omkar',20000,'2021-03-12'),
+('bunty',25000,'2021-04-18'),
+('gaurav',10000,'2020-05-13'),
+('Arun',30000,'2020-08-19');
+
+
+select * from empoyee_payrolls1;
