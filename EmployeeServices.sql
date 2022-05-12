78e5c1ba02ebe7e5fdedb4f9ebee17938d7ed4b1
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-13 01:20:54 +0530

    employee-payroll

--- EmployeeServices.sql
+++ EmployeeServices.sql
@@ -34,3 +34,22 @@ select Avg(salary) as "AverageSalary",Gender from empoyee_payrolls1 group by Gen
 select Min(salary) as "MinimumSalary",Gender from empoyee_payrolls1 group by Gender;
 select Max(salary) as "MaximumSalary",Gender from empoyee_payrolls1 group by Gender;
 select count(salary) as "CountSalary",Gender from empoyee_payrolls1 group by Gender;
+
+Alter table empoyee_payrolls1
+add EmployeePhoneNumber BigInt,EmployeeDepartment varchar(200) not null default 'Publish',Address varchar(200) default 'Not Provided';
+
+Update empoyee_payrolls1 
+set EmployeePhoneNumber='7774962478',EmployeeDepartment='Editing',Address='sangmeshwar'
+where name='omkar';
+
+Update empoyee_payrolls1 
+set EmployeePhoneNumber='7774962489',Address='mumbai'
+where name ='gaurav';
+
+Update empoyee_payrolls1 
+set EmployeePhoneNumber='7775862478',EmployeeDepartment='Management',Address='pune'
+where name ='bunty';
+
+Update empoyee_payrolls1 
+set EmployeePhoneNumber='8884962478',Address='devrukh'
+where name ='Arun';
