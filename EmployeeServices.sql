e9d0427ff06a7f3f09805d8e67464f756a79a457
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-13 01:29:32 +0530

    Duplicate-Value

--- EmployeeServices.sql
+++ EmployeeServices.sql
@@ -18,4 +18,10 @@ update empoyee_payroll
 set NetPay=(BasicPay - Deduction)
 update empoyee_payrolls1
 set TaxablePay=0,IncomeTax=0
-select * from empoyee_payroll;
+select * from empoyee_payrolls1;
+
+
+------ UC 10: Adding another Value for Rujula in Editing Department -------
+
+Insert into empoyee_Payrolls1(name,BasicPay,StartDate,Address,EmployeePhoneNumber,EmployeeDepartment) values ('omkar',250000,'2019-04-20','mumbai','7774962478','Editing');
+select * from empoyee_payrolls1;
