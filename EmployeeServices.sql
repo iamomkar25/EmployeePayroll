40601371d86a64d70ce827c148dbd17b716e540f
omkar Rakesh Shinde <omkar25shinde@gmail.com>
2022-05-13 01:05:31 +0530

    addGender

--- EmployeeServices.sql
+++ EmployeeServices.sql
@@ -14,3 +14,15 @@ select * from empoyee_payrolls1;
 ------ UC 5: Select Query using Cast() an GetDate() -------
 select salary from empoyee_payrolls1 where name='omkar';
 select salary from empoyee_payrolls1 where startDate BETWEEN Cast('2020-12-20' as Date) and GetDate();
+
+------ UC 6: Add Gender Column and Update Table Values -------
+Alter table empoyee_payrolls1
+add Gender char(1);
+
+Update empoyee_payrolls1 
+set Gender='M'
+where name='omkar';
+
+Update empoyee_payrolls1 
+set Gender='F'
+where name='gaurav' or name='Shalini'or name='Arun';
